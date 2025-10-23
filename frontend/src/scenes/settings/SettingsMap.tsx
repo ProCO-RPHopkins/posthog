735ea17ba3292e68a2af1173af887064021a1d65
jovan sakovic <jovan@posthog.com>
2025-10-23 22:36:58 +0100

    feat: add project secret api keys

--- frontend/src/scenes/settings/SettingsMap.tsx
+++ frontend/src/scenes/settings/SettingsMap.tsx
@@ -90,6 +90,7 @@ import { OrganizationLogo } from './organization/OrgLogo'
 import { OrganizationDangerZone } from './organization/OrganizationDangerZone'
 import { OrganizationSecuritySettings } from './organization/OrganizationSecuritySettings'
 import { VerifiedDomains } from './organization/VerifiedDomains/VerifiedDomains'
+import { ProjectAPIKeys } from './project/ProjectAPIKeys'
 import { ProjectDangerZone } from './project/ProjectDangerZone'
 import { ProjectMove } from './project/ProjectMove'
 import { ProjectDisplayName } from './project/ProjectSettings'
@@ -135,6 +136,12 @@ export const SETTINGS_MAP: SettingSection[] = [
                 id: 'variables',
                 title: 'Project ID',
                 component: <TeamVariables />,
+            },
+            {
+                id: 'environment-secret-api-keys',
+                title: 'Project secret API keys',
+                component: <ProjectAPIKeys />,
+                flag: 'PROJECT_SECRET_API_KEYS',
             },
         ],
     },
