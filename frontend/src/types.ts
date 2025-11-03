4f3820fe61f6498454f6853f7508d69cde7f9d52
Yasen Slavov <yasen@posthog.com>
2025-11-03 16:00:14 +0200

    Add access control to activity logs

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -312,6 +312,7 @@ export enum AccessControlResourceType {
     Survey = 'survey',
     Experiment = 'experiment',
     WebAnalytics = 'web_analytics',
+    ActivityLog = 'activity_log',
 }
 
 interface UserBaseType {
